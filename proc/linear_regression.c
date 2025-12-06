37ce1494f9ebe536a31f1087fcc61f192866fbf4
haonanc00 <haonanc123@gmail.com>
2025-12-05 17:05:03 -0800

    Update linear_regression.c

--- proc/linear_regression.c
+++ proc/linear_regression.c
@@ -3,36 +3,37 @@
 #include <stdlib.h>
 #include "linear_regression.h"
 
+// Train linear regression using gradient descent
 void linear_regression_fit(Frame *X, double *y, double *w_out, double *b_out) {
     int n = X->rows;
     int d = X->cols;
-    
     double lr = 0.01;
     int epochs = 1000;
-    
+
     for (int j = 0; j < d; j++) w_out[j] = 0.0;
     *b_out = 0.0;
-    
+
     for (int epoch = 0; epoch < epochs; epoch++) {
         double *grad_w = calloc(d, sizeof(double));
         double grad_b = 0.0;
-        
+
         for (int i = 0; i < n; i++) {
             double pred = *b_out;
             for (int j = 0; j < d; j++) pred += X->data[i][j] * w_out[j];
             double err = pred - y[i];
-            
+
             grad_b += err;
             for (int j = 0; j < d; j++) grad_w[j] += err * X->data[i][j];
         }
-        
+
         *b_out -= lr * grad_b / n;
         for (int j = 0; j < d; j++) w_out[j] -= lr * grad_w[j] / n;
-        
+
         free(grad_w);
     }
 }
 
+// Predict values based on learned weights and bias
 void linear_regression_predict(Frame *X, double *w, double b, double *out) {
     for (int i = 0; i < X->rows; i++) {
         double s = b;
